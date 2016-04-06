0cf07a75d93289cd738c67e5df898e9ab27547f1
xhuy0117 <chuy_punk@hotmail.com>
2016-04-06 11:28:29 -0500

    Adding fabric.js

--- arkSkillTreeModule.js
+++ arkSkillTreeModule.js
@@ -1,39 +1,46 @@
 var arkSkillTreeModule = (function() {
-    // private 
-    var cookingItems;
-    var cookingImages;
-    return {
-        // public
-        init: function() {
-            
-            $.getJSON('data/cooking.json', function(data) {
-                alert(data);
-                cookingItems = data;
-            });
-            
-            cookingImages = [];
-            for (var i = 0; i < cookingItems.length; i++) {
-                var image = new Image();
-                image.src = cookingItems[i].image;
-                cookingImages.push(image);
-            }
-        },
-        
-        getCookingImages: function() {
-            return cookingImages;
-        }
-    };
+	// private
+	var cookingItems;
+	var cookingImages;
+	return {
+		// public
+		init : function() {
+
+			$.getJSON('data/cooking.json', function(data) {
+				cookingItems = data;
+				cookingImages = [];
+				for (var i = 0; i < cookingItems.length; i++) {
+					var image = new Image();
+					image.src = cookingItems[i].image;
+					cookingImages.push(image);
+				}
+				var canvas = new fabric.Canvas('canvasArkEngramTree');
+
+				var images = arkSkillTreeModule.getCookingImages();
+
+				for (var i = 0; i < images.length; i++) {
+					var imgInstance = new fabric.Image(images[i], {
+						left : 10,
+						top : 20,
+						angle : 0,
+						opacity : 0.85
+					});
+					canvas.add(imgInstance);
+				}
+			});
+		},
+
+		getCookingImages : function() {
+			return cookingImages;
+		}
+	};
 })();
 
 $(document).ready(function() {
-    arkSkillTreeModule.init();
-    
-    var c = document.getElementById("canvasArkEngramTree");
-    var ctx = c.getContext("2d");
-    
-    var images = arkSkillTreeModule.getCookingImages();
-    
-    for (var i = 0; i < images.length; i++) {
-        ctx.drawImage(images[i], 10, 10, 256, 256);
-    }
+	arkSkillTreeModule.init();
+	
+	var canvas = new fabric.Canvas('canvasArkEngramTree');
+	fabric.Image.fromURL('images/00001-Campfire.png', function(oImg) {
+		  canvas.add(oImg);
+		});
 });
