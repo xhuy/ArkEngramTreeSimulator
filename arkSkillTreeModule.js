284bfa356d00d4597b2f19fd9b4ad12dceb10b2e
xhuy0117 <chuy_punk@hotmail.com>
2016-04-12 17:09:16 -0500

    Changes in JSON files to store more information about the graph, changes in JS to draw lines and also add the fabric.line objects to the array of objects, adding svg (final look of the GUI). changei in index in tittle

--- arkSkillTreeModule.js
+++ arkSkillTreeModule.js
@@ -2,9 +2,31 @@ var arkSkillTreeModule = (function() {
 	// private
 	var cookingItems;
 	var toolsItems;
+	var armorItems;
+	var structureItems;
+	var weaponsItems;
+	var canvas;
+	var offsetLines = 25;
 	return {
 		// public
 		init : function() {
+
+			arkSkillTreeModule.canvas = new fabric.Canvas(
+					'canvasArkEngramTree', {
+						selection : false
+					});
+
+			$.ajax({
+				dataType : "json",
+				url : 'data/armor.json',
+				async : true
+			}).done(function(data, textStatus, jqXHR) {
+				armorItems = data;
+				arkSkillTreeModule.drawImages(armorItems);
+				// arkSkillTreeModule.drawLines(armorItems);
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+				alert(textStatus + " in armor.json");
+			});
 
 			$.ajax({
 				dataType : "json",
@@ -14,7 +36,19 @@ var arkSkillTreeModule = (function() {
 				cookingItems = data;
 				arkSkillTreeModule.drawImages(cookingItems);
 			}).fail(function(jqXHR, textStatus, errorThrown) {
-				alert(textStatus);
+				alert(textStatus + " in cooking.json");
+			});
+
+			$.ajax({
+				dataType : "json",
+				url : 'data/structures.json',
+				async : true
+			}).done(function(data, textStatus, jqXHR) {
+				structureItems = data;
+				arkSkillTreeModule.drawImages(structureItems);
+				arkSkillTreeModule.drawLines(structureItems);
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+				alert(textStatus + " in structures.json");
 			});
 
 			$.ajax({
@@ -25,19 +59,107 @@ var arkSkillTreeModule = (function() {
 				toolsItems = data;
 				arkSkillTreeModule.drawImages(toolsItems);
 			}).fail(function(jqXHR, textStatus, errorThrown) {
-				alert(textStatus);
+				alert(textStatus + " in tools.json");
+			});
+
+			$.ajax({
+				dataType : "json",
+				url : 'data/weapons.json',
+				async : true
+			}).done(function(data, textStatus, jqXHR) {
+				weaponsItems = data;
+				arkSkillTreeModule.drawImages(weaponsItems);
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+				alert(textStatus + " in weapons.json");
+			});
+
+			// var canvas = new fabric.Canvas('canvasArkEngramTree');
+			// fabric.loadSVGFromURL('http://localhost:8080/Ark/docs/engram_tree.svg',
+			// function(objects, options) {
+			// var obj = fabric.util.groupSVGElements(objects, options);
+			// canvas.add(obj).renderAll();
+			// });
+
+			arkSkillTreeModule.canvas.on('object:moving', function(e) {
+				var image = e.target;
+				console
+						.log("Element ID: " + image.id + ", Type: "
+								+ image.type);
+
+				var objectMoving = arkSkillTreeModule.findByIdOrName(
+						image.type, image.id);
+
+				$.each(objectMoving.unlocksLines, function(index, value) {
+					value.set({
+						'x1' : image.left + offsetLines,
+						'y1' : image.top + offsetLines
+					})
+
+				});
+				arkSkillTreeModule.canvas.renderAll();
 			});
 
 		},
 
 		drawImages : function(data) {
-			var canvas = new fabric.Canvas('canvasArkEngramTree');
 
 			$.each(data, function(index, value) {
-				fabric.Image.fromURL(value.image, function(image) {
-					canvas.add(image);
+				fabric.Image.fromURL(value.imageURL, function(image) {
+					value.image = image;
+					if (value.id !== null) {
+						image.id = value.id;
+						image.type = value.type;
+					} else {
+						image.id = value.name;
+						image.type = value.type;
+					}
+					image.left = value.imageX;
+					image.top = value.imageY;
+					image.hasControls = image.hasBorders = false;
+					arkSkillTreeModule.canvas.add(image);
 				});
 			});
+		},
+
+		drawLines : function(jsonData) {
+			$.each(jsonData, function(index, father) {
+				$.each(father.unlocks, function(index, sonArray) {
+					var son = arkSkillTreeModule.findByIdOrName(sonArray[0],
+							sonArray[1]);
+
+					if (son !== undefined) {
+						var coords = [];
+						coords.push(father.imageX + offsetLines);
+						coords.push(father.imageY + offsetLines);
+						coords.push(son.imageX + offsetLines);
+						coords.push(son.imageY + offsetLines);
+
+						var line = new fabric.Line(coords, {
+							fill : 'red',
+							stroke : 'red',
+							strokeWidth : 5,
+							selectable : false
+						});
+
+						father.unlocksLines.push(line);
+
+						arkSkillTreeModule.canvas.add(line);
+					}
+				});
+			});
+		},
+
+		findByIdOrName : function(typeItem, idOrName) {
+
+			if (typeItem === "structures") {
+				var indexFinded = -1;
+				$.each(structureItems, function(index, item) {
+					if (item.id === idOrName || item.name === idOrName) {
+						indexFinded = index;
+					}
+				});
+				return structureItems[indexFinded];
+			}
 		},
 
 		getCookingItems : function() {
